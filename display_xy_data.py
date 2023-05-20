20ef621017ff648abfd412fcdd079cd047208ec2
shinter61 <shinter61@gmail.com>
2023-05-20 21:50:11 +0900

    20230520

---
+++ display_xy_data.py
@@ -0,0 +1,22 @@
+import csv
+import numpy as np
+
+with open('./0517_170409.csv') as f:
+  reader = csv.reader(f)
+
+  # l に csv の中身が二次元配列で入る
+  l = [row for row in reader]
+
+  # 上から4行削除
+  for _i in range(4):
+    l.pop(0)
+
+  l = np.array(l)
+
+  # axis = 1 なら列を削除
+  temperature_data = np.delete(l, 0, axis=1)
+
+  # 二次元配列
+  for x in range(192):
+    for y in range(256):
+       print(f'x: {x + 1}, y: {y + 1}, value: {temperature_data[y][x]}')
